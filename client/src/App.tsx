0a9e7e1547feb83026d9f071bb0ab4f9490a5c51
Sergiu Plesco <plescosergiu@gmail.com>
2024-04-17 20:58:50 +0300

    client set up, installed necessary libaries

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1>Orange Book Store</h1>
+    </div>
+  );
+}
+
+export default App;
